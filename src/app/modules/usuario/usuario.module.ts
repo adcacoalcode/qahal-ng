416fad3c53d431c5b022f6acc445e17a59d39fb1
Ostrowskii <vanessaostroski08@gmail.com>
2023-01-12 15:45:29 -0400

    adicionado componente usuario-list

--- src/app/modules/usuario/usuario.module.ts
+++ src/app/modules/usuario/usuario.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UsuarioRoutingModule } from './usuario-routing.module';
 import { UsuarioFormComponent } from './components/usuario-form/usuario-form.component';
+import { UsuarioListComponent } from './components/usuario-list/usuario-list.component';
 
 
 @NgModule({
   declarations: [
-    UsuarioFormComponent
+    UsuarioFormComponent,
+    UsuarioListComponent
   ],
   imports: [
     CommonModule,
