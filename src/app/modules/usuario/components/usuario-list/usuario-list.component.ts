3c964e8914c32a82bb66b193a2958e3a0e3bf0d6
Ostrowskii <vanessaostroski08@gmail.com>
2023-01-12 18:16:45 -0400

    desenvolvido componente usuario list

--- src/app/modules/usuario/components/usuario-list/usuario-list.component.ts
+++ src/app/modules/usuario/components/usuario-list/usuario-list.component.ts
@@ -1,15 +1,111 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+import { User } from 'src/app/core/models/user.model';
+import { AccountService } from 'src/app/core/services/account.service';
+import { UserService } from 'src/app/core/services/user.service';
+import { UsuarioFormComponent } from '../usuario-form/usuario-form.component';
 
 @Component({
   selector: 'app-usuario-list',
   templateUrl: './usuario-list.component.html',
-  styleUrls: ['./usuario-list.component.scss']
+  styleUrls: ['./usuario-list.component.scss'],
+  providers: [DialogService],
 })
+
 export class UsuarioListComponent implements OnInit {
+  regs: User[] = [];
 
-  constructor() { }
+  reg: User;
 
-  ngOnInit(): void {
-  }
+  selectedRegs: User[];
+  loading: boolean = false;
+
+  cols: any[];
+  userInfo: any;
+
+  constructor(private messageService: MessageService, private confirmationService: ConfirmationService,
+    private service: UserService, private dialogService: DialogService,
+    private accountService: AccountService) { }
+
+    ngOnInit() {
+      this.userInfo = this.accountService.getUserInfo();
+
+      this.cols = [
+        { field: 'nome', header: 'Nome', width: '85%' },
+        { field: 'acao', header: 'Ações', width: '15%' },
+      ];
+
+      this.updateUI();
+    }
+
+    async updateUI() {
+      this.loading = true;
+      this.service.getAllByIgrejagrupo(this.userInfo.igrejagrupo.id).subscribe({
+        next: (regs) => {
+          this.regs = regs;
+          this.loading = false;
+        },
+        error: (error) => {
+          this.loading = false;
+          this.messageService.add({ severity: 'error', summary: 'Erro!', detail: 'Erro ao carregar registros', life: 3000 });
+        }
+      }
+      );
+    }
+
+    openNew() {
+      this.reg = {};
+      const ref = this.dialogService.open(UsuarioFormComponent, {
+        data: {
+          id: undefined
+        },
+        header: 'Usuário',
+        width: '70%'
+      });
+
+      ref.onClose.subscribe(res => {
+        console.log(res)
+        this.updateUI();
+      });
+    }
+
+    edit(reg: User) {
+      this.reg = { ...reg };
+      const ref = this.dialogService.open(UsuarioFormComponent, {
+        data: {
+          id: reg.id
+        },
+        header: 'Usuário',
+        width: '70%'
+      });
+
+      ref.onClose.subscribe(res => {
+        console.log(res)
+        this.updateUI();
+      });
+    }
+
+    delete(reg: User) {
+      this.confirmationService.confirm({
+        message: 'Tem certeza que deseja excluir esse registro?',
+        header: 'Confirmar',
+        icon: 'pi pi-exclamation-triangle',
+        acceptLabel: 'Sim',
+        rejectLabel: 'Não',
+        accept: () => {
+          this.regs = this.regs.filter(val => val.id !== reg.id);
+          this.service.delete(reg.id).subscribe({
+            next: () => {
+              this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Registro excluído com sucesso', life: 3000 });
+              this.updateUI();
+            },
+            error: () => {
+              this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao excluir registro', life: 3000 });
+            },
+          });
+        }
+      });
+    }
 
 }
